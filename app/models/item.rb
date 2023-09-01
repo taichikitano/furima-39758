5723f3295a1eda43a59607a39bfcddaf2d1cf6ce
taichikitano <taichikitano@gmail.com>
2023-09-01 15:33:57 +0900

    itemモデル、テーブル作成

---
+++ app/models/item.rb
@@ -0,0 +1,10 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  #has_one :purchase
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_fee_responsibility
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :lead_time
+
+end
