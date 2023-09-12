669ebba9778bdfbcecc0d724fd4521ef5b5b1d21
taichikitano <taichikitano@gmail.com>
2023-09-12 19:25:04 +0900

    エラー文の日本語化

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -5,9 +5,9 @@ class PurchaseShipping
   
   with_options presence: true do
     validates :user_id, :item_id, :city, :address, :token
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
-    validates :phone_number, numericality: { only_integer: true, message: "is invalid"  }, length: { minimum: 10, maximum: 11, message: "must be between 10 and 11 digits" }
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "は不正な値です ※ハイフンを含める必要があります"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "を選択してください"}
+    validates :phone_number, numericality: { only_integer: true, message: "は不正な値です"  }, length: { minimum: 10, maximum: 11, message: "は10文字以上11文字以内で入力してください" }
   end
 
   def save
